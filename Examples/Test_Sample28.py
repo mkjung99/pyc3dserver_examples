9e0dd99d751211b08712eae4600782e6dfbe7ad3
Moon Ki Jung <m.k.jung@outlook.com>
2020-06-24 20:30:18 +0100

    Create Test_Sample28.py
    Add a new example.

---
+++ Examples/Test_Sample28.py
@@ -0,0 +1,24 @@
+import os
+import sys
+if sys.path[0] != os.getcwd():
+    sys.path.insert(0, os.getcwd())
+lib_local_path = os.path.normpath(r'C:\WORKSPACE\DEV\pyc3dserver')
+if os.path.exists(lib_local_path):
+    if sys.path[1] != lib_local_path:
+        sys.path.insert(1, lib_local_path)
+import pyc3dserver as c3d
+import numpy as np
+#%%
+c3d_sample_dir_path = os.path.join(os.getcwd(), r'..\Samples_C3D\Sample28')
+src_c3d_path = os.path.join(c3d_sample_dir_path, 'dynamic.c3d')
+
+itf = c3d.c3dserver()
+c3d.init_logger(logger_lvl='DEBUG', c_hdlr_lvl='DEBUG', f_hdlr_lvl='DEBUG', f_hdlr_f_path=None)
+
+ret = c3d.open_c3d(itf, src_c3d_path)
+dict_header = c3d.get_dict_header(itf)
+dict_groups = c3d.get_dict_groups(itf)
+dict_markers = c3d.get_dict_markers(itf, log=True)
+dict_forces = c3d.get_dict_forces(itf, desc=True, frame=True, time=True, log=True)
+dict_analogs = c3d.get_dict_analogs(itf, desc=True, log=True)
+ret = c3d.close_c3d(itf)
